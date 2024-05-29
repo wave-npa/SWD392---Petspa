cba7d3cedf26e09d176594c2dfda670d0d42ae15
WaveInf <ngnphucan@gmail.com>
2024-05-29 15:59:17 +0700

    fix repo

--- src/main/java/org/petspa/petcaresystem/repository/CustomerRepository.java
+++ src/main/java/org/petspa/petcaresystem/repository/CustomerRepository.java
@@ -1,5 +1,10 @@
 package org.petspa.petcaresystem.repository;
 
-public class CustomerRepository {
+import org.petspa.petcaresystem.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, String>{
 
 }
